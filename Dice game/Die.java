bebf6705cf2da761be57c37c83765d9259815f11
V2S2P <157969468+V2S2P@users.noreply.github.com>
2024-09-29 18:09:42 +0200

    Update Die.java

--- Dice game/Die.java
+++ Dice game/Die.java
@@ -5,4 +5,11 @@ public class Die{
 	int throwDice(int sides){
 		return (int)(Math.random() * sides) + 1;
 	}
-}
+	
+	int resultOfDieThrow(int result1, int result2){
+		if(result1 > result2){
+			return result1;
+		}else{
+			return result2;
+		}
+	}
