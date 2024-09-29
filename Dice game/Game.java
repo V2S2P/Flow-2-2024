aecb7b5198c38760fc64bd9c78386156003b1f35
V2S2P <157969468+V2S2P@users.noreply.github.com>
2024-09-29 18:09:29 +0200

    Update Game.java

--- Dice game/Game.java
+++ Dice game/Game.java
@@ -1,7 +1,20 @@
 public class Game{
 	public static void main(String[] args){
 		Die die = new Die();
+		Die die1 = new Die();
 
-		System.out.println(die.throwDice(6));
+		int result1 = die.throwDice(6);
+		int result2 = die1.throwDice(6);
+
+		System.out.println("Die 1: " + result1);
+		System.out.println("Die 2: " + result2);
+
+	
+		if(result1==result2){
+			System.out.println("It's a tie!");
+		}else{
+			int higher = die.resultOfDieThrow(result1, result2);
+			System.out.println("The higher roll is: " + higher);
+		}
 	}
 }
