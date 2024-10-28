fb42ef5bdd4c90b799adfc8d46952fbbd2a8aeaf
V2S2P <157969468+V2S2P@users.noreply.github.com>
2024-10-28 13:18:21 +0100

    Add files via upload

---
+++ Maven/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.abstractica.javacsg.Geometry2D;
+import org.abstractica.javacsg.Geometry3D;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        JavaCSG csg = JavaCSGFactory.createDefault();
+
+
+
+    }
+}
